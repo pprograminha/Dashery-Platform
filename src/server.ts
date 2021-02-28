9c151543713cd215cc7fe2faf8a45d924f1a2fc8
x14102004x <marcosproenca144@gmail.com>
2021-02-28 19:55:02 -0300

    Created Server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,3 @@
-import express from "express";
-const app = express();
-app.get("/", (req, res) => {
-    return res.send("Helloe World!");
-});
-app.listen(3333, () => {
-    console.log("Listening to port 3333");
-});
+import { app } from "./app";
+
+app.listen(3333, () => console.log("Server is running"));
