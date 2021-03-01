796c01b3cf9bfbf95e96925b45de3610e04644a2
x14102004x <marcosproenca144@gmail.com>
2021-03-01 13:39:38 -0300

    Updated App

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { router } from "./routes";
+import path from "path";
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(express.static("public"));
+app.use("/static", express.static(path.join(__dirname + "../public")));
 
 export { app };
