86f8f2eff928d3cfff24e2c593d36a10f634bc65
x14102004x <marcosproenca144@gmail.com>
2021-03-02 21:05:48 -0300

    Added Migrations

---
+++ src/database/migrations/1614720802498-CreateDepositions.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateDepositions1614720802498 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "depositions",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "username",
+            type: "varchar",
+          },
+          {
+            name: "deposition",
+            type: "text",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("depositions");
+  }
+}
