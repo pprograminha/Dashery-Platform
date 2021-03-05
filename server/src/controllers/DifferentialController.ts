4a7352a5775653d35b2dede105449342c0802080
x14102004x <marcosproenca144@gmail.com>
2021-03-04 22:49:32 -0300

    Updated Database

--- server/src/controllers/DifferentialController.ts
+++ server/src/controllers/DifferentialController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import { Differentials } from '../models/Differentials'
 
 export class DifferentialController {
    async show(req: Request, res: Response) {}
-   async create(req: Request, res: Response) {}
+   async create(req: Request, res: Response) {
+      const { icon, title, description } = req.body
+      const differentialRepository = getRepository(Differentials)
+      const differentials = differentialRepository.create({
+         icon,
+         title,
+         description,
+      })
+
+      await differentialRepository.save(differentials)
+      res.send(req.body)
+   }
    async destroy(req: Request, res: Response) {}
    async update(req: Request, res: Response) {}
 }
