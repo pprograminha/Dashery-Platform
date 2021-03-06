1d980dd11fbd3d5dcf3f799078902ffc93004adb
x14102004x <marcosproenca144@gmail.com>
2021-03-06 17:51:14 -0300

    Updated Differential

--- server/src/controllers/DifferentialController.ts
+++ server/src/controllers/DifferentialController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express'
-import { getRepository } from 'typeorm'
-import { Differentials } from '../models/Differentials'
+import { getCustomRepository } from 'typeorm'
+import { DifferentialRepository } from './../repositories/DifferentialRepository'
 
 export class DifferentialController {
-   async show(req: Request, res: Response) {}
+   async show(req: Request, res: Response) {
+      const differentialRepository = getCustomRepository(DifferentialRepository)
+      const differentials = await differentialRepository.find()
+
+      res.status(200).send(differentials)
+   }
    async create(req: Request, res: Response) {
       const { icon, title, description } = req.body
-      const differentialRepository = getRepository(Differentials)
+      const differentialRepository = getCustomRepository(DifferentialRepository)
+      
       const differentials = differentialRepository.create({
          icon,
          title,
@@ -14,8 +20,24 @@ export class DifferentialController {
       })
 
       await differentialRepository.save(differentials)
-      res.send(req.body)
+      res.status(201).send(req.body)
    }
-   async destroy(req: Request, res: Response) {}
-   async update(req: Request, res: Response) {}
+   async destroy(req: Request, res: Response) {
+      const { id } = req.params
+      const differentialRepository = getCustomRepository(DifferentialRepository)
+
+      await differentialRepository.delete(id)
+
+      res.status(200).send('deleted')
+   }
+   async update(req: Request, res: Response) {
+      const { id } = req.params
+      const { icon, title, description } = req.body
+
+      const differentialRepository = getCustomRepository(DifferentialRepository)
+
+      await differentialRepository.update({ id }, { icon, title, description })
+
+      res.status(200).send(req.body)
+   }
 }
