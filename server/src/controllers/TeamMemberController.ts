47f447a051addde6d551bf7ed3823d0b75b6a4ae
x14102004x <marcosproenca144@gmail.com>
2021-03-07 15:31:28 -0300

    Updated TeamMember

--- server/src/controllers/TeamMemberController.ts
+++ server/src/controllers/TeamMemberController.ts
@@ -8,13 +8,55 @@ export class TeamMemberController {
       const teamMemberRepository = getCustomRepository(TeamMemberRepository)
       const teamMembers = await teamMemberRepository.find()
 
-      res.status(200).send(teamMembers)
+      return res.status(200).json(teamMembers)
    }
    async create(req: Request, res: Response) {
       const { title, description_one } = <TeamMember>req.body
+
       const teamMemberRepository = getCustomRepository(TeamMemberRepository)
-      teamMemberRepository.create({ title, description_one })
+
+      try {
+         const member = teamMemberRepository.create({ title, description_one })
+
+         await teamMemberRepository.save(member)
+
+         return res.status(201).json(member)
+      } catch (error) {
+         return res.status(400).json({ msg: error })
+      }
    }
-   async destroy(req: Request, res: Response) {}
-   async update(req: Request, res: Response) {}
+   async destroy(req: Request, res: Response) {
+      const { id } = req.params
+      const teamMemberRepository = getCustomRepository(TeamMemberRepository)
+
+      const member = await teamMemberRepository.findOne(id)
+      await teamMemberRepository.delete(id)
+
+      if (!member) return res.status(400).json({ msg: 'not found' })
+      return res.status(200).json(member)
+   }
+   async update(req: Request, res: Response) {
+      const { id } = req.params
+      const { description_one, description_two, member_image, title } = <
+         TeamMember
+      >req.body
+
+      const teamMemberRepository = getCustomRepository(TeamMemberRepository)
+      const member = await teamMemberRepository.findOne({ id })
+
+      try {
+         await teamMemberRepository.update(
+            { id },
+            { description_one, description_two, member_image, title }
+         )
+         return res.status(200).json({
+            description_one,
+            description_two: `${description_two ? description_two : null}`,
+            member_image: `${member_image ? member_image : null}`,
+            title,
+         })
+      } catch (error) {
+         return res.status(400).json({ msg: error })
+      }
+   }
 }
