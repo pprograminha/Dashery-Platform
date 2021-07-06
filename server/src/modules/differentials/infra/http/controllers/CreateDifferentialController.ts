8ad443857ab69f22f6ad249be594bb26dfd1ce01
Marcos Proença <marcosproenca144@gmail.com>
2021-07-05 22:43:40 -0300

    style: the indentations were redone

--- server/src/modules/differentials/infra/http/controllers/CreateDifferentialController.ts
+++ server/src/modules/differentials/infra/http/controllers/CreateDifferentialController.ts
@@ -3,18 +3,18 @@ import { getCustomRepository } from 'typeorm'
 import DifferentialsRepository from '../../typeorm/repositories/DifferentialsRepository'
 
 export default class CreateDifferentialController {
-   async handle(request: Request, response: Response): Promise<Response> {
-      const { title, description } = request.body
-      const differentialsRepository = getCustomRepository(DifferentialsRepository)
-      try {
-         const differential = await differentialsRepository.create({
-            title,
-            description,
-         })
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { title, description } = request.body
+    const differentialsRepository = getCustomRepository(DifferentialsRepository)
+    try {
+      const differential = await differentialsRepository.create({
+        title,
+        description,
+      })
 
-         return response.status(201).json(differential)
-      } catch (error) {
-         return response.status(400).json({ msg: error })
-      }
-   }
+      return response.status(201).json(differential)
+    } catch (error) {
+      return response.status(400).json({ msg: error })
+    }
+  }
 }
--- server/src/modules/differentials/infra/http/controllers/DestroyDifferentialController.ts
+++ server/src/modules/differentials/infra/http/controllers/DestroyDifferentialController.ts
@@ -3,16 +3,16 @@ import { getCustomRepository } from 'typeorm'
 import DifferentialsRepository from '../../typeorm/repositories/DifferentialsRepository'
 
 export default class DestroyDifferentialController {
-   async handle(req: Request, res: Response) {
-      const { differential_id } = req.body
-      const differentialRepository = getCustomRepository(DifferentialsRepository)
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { differential_id } = request.body
+    const differentialRepository = getCustomRepository(DifferentialsRepository)
 
-      const differential = await differentialRepository.listById(differential_id)
+    const differential = await differentialRepository.findById(differential_id)
 
-      if (!differential) return res.status(400).json({ msg: 'not found' })
+    if (!differential) return response.status(400).json({ msg: 'not found' })
 
-      await differentialRepository.destroyById(differential_id)
+    await differentialRepository.destroyById(differential_id)
 
-      return res.status(200).json(req.body)
-   }
+    return response.status(200).json(differential)
+  }
 }
